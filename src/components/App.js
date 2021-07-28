07aeb33cb9d07c03ec5987a61d04896c7a236a82
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-28 09:43:15 -0400

    initial commit fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(plantData => console.log(plantData))
+  })
+
   return (
     <div className="app">
       <Header />
