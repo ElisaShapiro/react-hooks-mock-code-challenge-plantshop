babddcfa4cbabd3358b2a0529acc0122d11f454f
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-28 10:23:40 -0400

    deliv 1 2 3 done

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ addPlant }) {
+  const  [name, setName] = useState("")
+  const [image, setImage] = useState("")
+  const [price, setPrice] = useState("") 
+  
+  function makeNewPlant(e){
+    e.preventDefault()
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-type" : "application/json"
+      }, 
+      body: JSON.stringify({
+        name,
+        image,
+        price,
+      })
+    }).then(response => response.json())
+    .then(newPlant => addPlant(newPlant)) 
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
-        <button type="submit">Add Plant</button>
+      <form onSubmit={makeNewPlant}>
+        <input onChange={(e) => setName(e.target.value)} value={name}
+          type="text" name="name" placeholder="Plant name" />
+        <input onChange={(e) => setImage(e.target.value)} value={image}
+          type="text" name="image" placeholder="Image URL" />
+        <input onChange={(e) => setPrice(e.target.value)} value={price}
+          type="number" name="price" step="0.01" placeholder="Price" />
+        <button 
+          type="submit">Add Plant</button>
       </form>
     </div>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage({ plants, setPlants }) {
+  const [input, setInput] = useState("")
+
+  const addPlant = newPlant => {
+    setPlants(currentPlants => {
+      const newPlants = [...currentPlants]
+      newPlants.push(newPlant)
+      return newPlants
+    })
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm 
+        addPlant={addPlant}
+      />
       <Search />
       <PlantList 
         plants={plants} 
