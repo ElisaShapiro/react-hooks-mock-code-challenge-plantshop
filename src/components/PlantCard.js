d25f7015a9715c0f45823d22d4d8c37620dc018f
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-28 09:53:27 -0400

    deliv 1 and 3

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
+  const [stock, setStock] = useState(true)
+
   return (
     <li className="card">
       <img src={plant.image} alt={"plant name"} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {stock ? (
+        <button onClick={(e) => setStock(!stock)} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={(e) => setStock(!stock)} >Out of Stock</button>
       )}
     </li>
   );
